2e85177b9adba83a3cecda87f5cc11f0ead81e0d
gaurav mandal <avdhika@gmail.com>
2023-06-10 13:50:57 +0530

    for production

--- app.cjs
+++ app.cjs
@@ -17,19 +17,5 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/carts', cartRouter);
 
-app.use(express.static(path.join(__dirname , "./Frontend/dist")))
-// app.use('./api/v1/bookings' , bookingRoutes )
 
-app.get("*" , (req, res)=>{
-    res.sendFile(path.join(__dirname , "./Frontend/dist/index.html"))
-   
-})
-app.all('*',( req, res, next) => {
- 
-    next(new AppError(`cant find the ${req.originalUrl} on this server`,404));
-   })
-   
-   
-   // global error handling  mechanism 
-   app.use(globalErrorHandler)
 module.exports = app;
--- server.cjs
+++ server.cjs
@@ -1,8 +1,24 @@
 const app = require('./app.cjs')
 
 const express = require('express')
-const PORT_NO = process.env.port || 1200;
+const PORT_NO = process.env.PORT || 1200;
 
+
+app.use(express.static(path.join(__dirname , "./Frontend/dist")))
+// app.use('./api/v1/bookings' , bookingRoutes )
+
+app.get("*" , (req, res)=>{
+    res.sendFile(path.join(__dirname , "./Frontend/dist/index.html"))
+   
+})
+// app.all('*',( req, res, next) => {
+ 
+//     next(new AppError(`cant find the ${req.originalUrl} on this server`,404));
+//    })
+   
+   
+   // global error handling  mechanism 
+   app.use(globalErrorHandler)
 
 
 
