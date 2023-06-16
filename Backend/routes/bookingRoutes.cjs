c54c9f9eb627dc92a3d03cb814dce26cd6b0cf5b
gaurav mandal <avdhika@gmail.com>
2023-06-16 14:00:38 +0530

    frontend

--- Backend/routes/bookingRoutes.cjs
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const bookingController = require('../controllers/bookingController.cjs')
-const authcontroller = require('../controllers/authController.cjs')
-const router = express.Router();
-
-router.get('/checkout-session/:productId', authcontroller.protect, bookingController.getCheckoutSession)
